cd5dd27d45625a51e3c1e8c23b7e264ac3109513
unknown <you@example.com>
2022-07-01 20:32:17 +0700

    a

--- C0522G1_HoHaiHoc_Module2/src/ss1_Introduction_to_Java/exercise/Hello.java
+++ C0522G1_HoHaiHoc_Module2/src/ss1_Introduction_to_Java/exercise/Hello.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.print("Input your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Hello " + name);
     }
 }
